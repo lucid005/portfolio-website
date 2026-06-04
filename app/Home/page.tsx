b627146511b075067c22e4ce8eca6000f7183c07
lucid005 <sauravshrestha2005@gmail.com>
2026-06-04 16:49:31 +0545

    layout border

---
+++ app/Home/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="max-w-6xl w-full p-4 border h-screen">
+      <div className="border h-full flex items-center justify-center gap-4">
+        <div className="border h-full w-2/6">
+
+        </div>
+        <div className="border h-full w-4/6">
+
+        </div>
+      </div>
+    </main>
+  );
+}
